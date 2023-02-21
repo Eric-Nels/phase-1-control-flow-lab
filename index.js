fccc0a49a983242ba9dc0aceaa9fe8e266c6c8d0
Eric Nelson <ernelson12o0@gmail.com>
2023-02-21 13:03:47 -0800

    Lab Complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance < 400) {
+    return "This one is on me!"
+  } else if (distance < 2000) {
+    return "That will be twenty bucks."
+  } else if (distance >2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (distance > 2500) {
+    return "No can do."
+  }
+}
+  
+function ternaryCheckCity(city){
+  let location = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return location 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let message; 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much."
+      break;
+    case "not as generous":
+      message = "Thank you."
+      break;
+    default:
+      message = "Bye."
+      break;
+  }  
+  return message
 }
